5ee0ba34e306aaa936a8e0e4b38b2db61ca0f8c9
Your Name <you@example.com>
2024-03-27 20:10:56 +0000

    new try

--- list.c
+++ list.c
@@ -64,11 +64,10 @@ void * nextList(List * list)
 
 void * lastList(List * list) 
 {
-  if (list -> tail == NULL)
+  if (list -> tail != NULL)
   {
-    return NULL;
+    list -> current = list -> tail;
   }
-  list -> current = list -> tail;
   return list -> current -> data;
 }
 
